1e9a16b077dbb8d28615123fc7c0bb18460a8d92
ikurotime <davidhuertasortiz@gmail.com>
2022-07-05 15:49:33 +0200

    ➖ Remove(Flowbite)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}',
-  "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  darkMode: 'class',
 }

951aa8357a39fd4124226a05e63782d812f5202c
ikurotime <davidhuertasortiz@gmail.com>
2022-07-06 18:49:45 +0200

    ➕ Add(Sidebar)

---
+++ src/components/SideBarIcon.svelte
@@ -0,0 +1,11 @@
+<script>
+// @ts-nocheck
+  export let icon,tooltip;
+</script>
+
+<div class="sidebar-icon group">
+ <svelte:component this={icon}/>
+  <span class="sidebar-tooltip group-hover:scale-100">
+    {tooltip}
+  </span>
+</div>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,16 @@
+<script>
+import Divider from "./Index/Divider.svelte";
+
+import SideBarIcon from "./SideBarIcon.svelte";
+import MdAdd from 'svelte-icons/md/MdAdd.svelte'
+import MdHome from 'svelte-icons/md/MdHome.svelte'
+import MdSettings from 'svelte-icons/md/MdSettings.svelte'
+</script>
+<div class="fixed top-0 left-0 h-screen w-16 flex flex-col bg-neutral-900 shadow-lg">
+  <SideBarIcon icon={MdHome} tooltip="Home"/>
+  <Divider />
+  <SideBarIcon icon={MdAdd} tooltip="Chat 1"/>
+  <SideBarIcon icon={MdAdd} tooltip="Add Chat"/>
+  <Divider />
+  <SideBarIcon icon={MdSettings} tooltip="Settings"/>
+</div>

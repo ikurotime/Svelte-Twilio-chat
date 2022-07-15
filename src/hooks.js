ad3bad36cb5e24f7cc10afa450e84a4210729775
ikurotime <davidhuertasortiz@gmail.com>
2022-07-16 00:39:10 +0200

    ➕ Add(User servers to session)

--- src/hooks.js
+++ src/hooks.js
@@ -1,27 +1,32 @@
 // @ts-nocheck
-import cookie from 'cookie'
-  //These hooks get called with every request to the server.
-  //https://kit.svelte.dev/docs/hooks#handle
-export const handle = async ({event,resolve}) => {
-  // Grab the cookie
-  const cookies = await cookie.parse(event.request.headers.get('cookie') || '')
-  const session = cookies.session ? JSON.parse(cookies.session) : null
+import { supabase } from '$lib/supabaseClient';
+import cookie from 'cookie';
+//These hooks get called with every request to the server.
+//https://kit.svelte.dev/docs/hooks#handle
+export const handle = async ({ event, resolve }) => {
+	// Grab the cookie
+	const cookies = await cookie.parse(event.request.headers.get('cookie') || '');
+	const session = cookies.session ? JSON.parse(cookies.session) : null;
+	const { data, error } = await supabase
+		.from('servers')
+		.select('friendly_name, SID, channels(channel_friendly_name, channel_sid)');
 
-  // If the session is in the cookie, asign it to the SvelteKit's store in order to work with getStore()
-  event.locals.user = session 
-  ? {
-    access_token: session.access_token,
-    id: session.user.id
-  }
-  : null
+	// If the session is in the cookie, asign it to the SvelteKit's store in order to work with getStore()
+	event.locals.user = session
+		? {
+				access_token: session.access_token,
+				id: session.user.id,
+				servers: data
+		  }
+		: null;
 
-  // Asign the session in the cokies to the SvelteKit's store
-  event.locals.session = session ? session.user : null
-  const response = await resolve(event)
-  return response
-}
+	// Asign the session in the cokies to the SvelteKit's store
+	event.locals.session = session ? session.user : null;
+	const response = await resolve(event);
+	return response;
+};
 
 // https://kit.svelte.dev/docs/hooks#getsession
 export const getSession = async (event) => {
-  return event.locals.session
-}
+	return event.locals.session;
+};

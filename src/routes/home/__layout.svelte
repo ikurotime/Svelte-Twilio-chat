8d9c0c5f30cf9ea86ae513b3ee24e54dd31c7540
ikurotime <davidhuertasortiz@gmail.com>
2022-07-16 14:00:54 +0200

    ➕ Add(User name)

--- src/routes/home/__layout.svelte
+++ src/routes/home/__layout.svelte
@@ -1,5 +1,13 @@
 <script>
 	import Sidebar from '$lib/components/Sidebar/Sidebar.svelte';
+	import { userName } from '$lib/stores/store';
+	import { session } from '$app/stores';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		if (!$userName) {
+			userName.set($session.user_metadata.full_name);
+		}
+	});
 </script>
 
 <div class="bg-neutral-700 h-full w-full flex">

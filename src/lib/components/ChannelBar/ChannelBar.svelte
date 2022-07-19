500b9f81606de3fef96aacd5ade171524cdf5a7a
ikurotime <davidhuertasortiz@gmail.com>
2022-07-19 02:17:36 +0200

    🔨 Fix(Draggable Channel bar)

--- src/lib/components/ChannelBar/ChannelBar.svelte
+++ src/lib/components/ChannelBar/ChannelBar.svelte
@@ -1,9 +1,10 @@
 <script>
 	import { session } from '$app/stores';
 	import MdShare from 'svelte-icons/md/MdShare.svelte'
+	import { draggable } from '@neodrag/svelte'
 </script>
 
-<div class="channel-bar shadow-lg">
+<div class="channel-bar shadow-lg" use:draggable={{axis: 'none',bounds: {}}}>
 	<slot />
 	<div class="flex items-center gap-3 text-white mt-auto px-5 py-4 border-t-2 border-gray-900">
 		<img
